8582bd701ae972c42380ed5bf10869ff6b182baf
Christoph Gockel <github@christophgockel.de>
2014-11-21 13:40:29 +0000

    initial commit

---
+++ spec/speclj_unicornleap/core_spec.clj
@@ -0,0 +1,7 @@
+(ns speclj-unicornleap.core-spec
+  (:require [speclj.core :refer :all]
+            [speclj-unicornleap.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/speclj_unicornleap/core.clj
@@ -0,0 +1,1 @@
+(ns speclj-unicornleap.core)
