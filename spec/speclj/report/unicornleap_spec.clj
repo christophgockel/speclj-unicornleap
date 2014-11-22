79b368c77bdfa87925b174821c3c9297914aa93b
Christoph Gockel <github@christophgockel.de>
2014-11-22 23:09:31 +0000

    Leaping trolls and unicorns

--- spec/speclj/report/unicornleap_spec.clj
+++ spec/speclj/report/unicornleap_spec.clj
@@ -1,7 +1,32 @@
-(ns speclj.report.unicornleap
+(ns speclj.report.unicornleap_spec
   (:require [speclj.core :refer :all]
-            [speclj-unicornleap.core :refer :all]))
+            [speclj.reporting :refer [report-runs report-error]]
+            [speclj.results :refer [pass-result pending-result fail-result error-result]]
+            [speclj.report.unicornleap :as unicornleap]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "Unicornleap Reporter"
+  (with reporter (unicornleap/new-unicornleap-reporter))
+  (with executed-command (atom nil))
+
+  (around [it]
+    (with-redefs [unicornleap/execute (fn [command]
+                                        (reset! @executed-command command))]
+      (it)))
+
+  (it "leaps a unicorn when nothing was reported"
+    (report-runs @reporter [])
+    (should= "unicornleap" @@executed-command))
+
+  (it "leaps a unicorn when no failures or errors were reported"
+    (report-runs @reporter [(pass-result nil 1)
+                            (pending-result nil 1 nil)])
+    (should= "unicornleap" @@executed-command))
+
+  (it "leaps a troll when a failure was reported"
+    (report-runs @reporter [(fail-result nil 1 nil)])
+    (should= "trolleap" @@executed-command))
+
+  (it "leaps a troll when an error was reported"
+    (report-runs @reporter [(error-result nil)])
+    (should= "trolleap" @@executed-command)))
+
--- src/speclj/report/unicornleap.clj
+++ src/speclj/report/unicornleap.clj
@@ -1,1 +1,33 @@
-(ns speclj.report.unicornleap)
+(ns speclj.report.unicornleap
+  (:use [clojure.java.shell :only [sh]]
+        [speclj.results :only [fail? error?]])
+  (:import [speclj.reporting Reporter]))
+
+(defn- execute [command]
+  (.start (ProcessBuilder. [command])))
+
+(defn- leap [failures]
+  (if (= failures true)
+    (execute "trolleap")
+    (execute "unicornleap")))
+
+(defn- is-failure? [result]
+  (or (fail? result) (error? result)))
+
+(defn- contains-failure? [collection]
+  (= true (some is-failure? collection)))
+
+(deftype UnicornleapReporter []
+  Reporter
+  (report-message [this message])
+  (report-description [this description])
+  (report-pass [this result])
+  (report-pending [this result])
+  (report-fail [this result])
+  (report-runs [this results]
+    (leap (contains-failure? results)))
+  (report-error [this error]))
+
+(defn new-unicornleap-reporter []
+  (UnicornleapReporter.))
+
